fef1a43f5eac0f4931bf244d4dabb0fa9cc08afa
Mark Mossberg <mark.mossberg@gmail.com>
2014-03-06 23:37:34 -0500

    fish updates
    - add more positive thoughts to smile.py
    - improve right-side prompt

--- bin/smile.py
+++ bin/smile.py
@@ -5,7 +5,12 @@ import random
 print random.choice([
     "You're awesome",
     "Have a great day",
-    "Show 'em how it's done)",
+    "Show em how it's done",
     "Shoot for the moon",
-    "Be excellent"
+    "Be excellent",
+    "Hakuna Matata",
+    "Don't worry, be happy",
+    "Smile",
+    "Keep calm and HACK",
+    "Let's Go"
     ]) + '! :)'
