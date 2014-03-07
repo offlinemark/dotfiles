797b87171ed5fbd326e1a6b38f832d946fe8aa40
Mark Mossberg <mark.mossberg@gmail.com>
2014-03-06 23:37:34 -0500

    fish updates
    - add more positive thoughts to smile.py
    - improve right-side prompt

--- fish/config.symlink/fish/functions/fish_right_prompt.fish
+++ fish/config.symlink/fish/functions/fish_right_prompt.fish
@@ -1,4 +1,9 @@
 function fish_right_prompt
     # date "+%m/%d/%y %r"
+    set_color $fish_color_cwd
+    echo -n "}["
+    set_color blue
     date "+%r"
+    set_color $fish_color_cwd
+    echo "]"
 end
