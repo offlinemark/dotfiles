425a42ca5edaa67554f4ad897743ccbe7a7ec3a7
Mark Mossberg <mark.mossberg@gmail.com>
2014-05-24 00:03:31 -0400

    update misc bin

--- bin/smile.py
+++ bin/smile.py
@@ -4,6 +4,7 @@ import random
 
 print random.choice([
     "You're awesome",
+    "You're incredble",
     "Have a great day",
     "Show em how it's done",
     "Shoot for the moon",
@@ -13,5 +14,8 @@ print random.choice([
     "Smile",
     "Keep calm and HACK",
     "Let's Go",
-    "Take it easy"
+    "Take it easy",
+    "You got this",
+    "Go get em",
+    "Great job"
     ]) + '! :)'
