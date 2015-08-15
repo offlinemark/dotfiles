0af544cba5a008a7b4540e566cc85fc16f5e4075
Mark Mossberg <mark.mossberg@gmail.com>
2015-08-15 17:18:19 -0400

    dotfiles bankruptcy

--- fish/config.symlink/fish/config.fish
+++ fish/config.symlink/fish/config.fish
@@ -1,9 +1,10 @@
 source ~/.aliases
 
 # env variables
-set -g -x PATH /usr/local/bin /usr/local/sbin $PATH ~/.dotfiles/bin /usr/local/heroku/bin
+set -g -x PATH . /usr/local/bin /usr/local/sbin $PATH ~/.dotfiles/bin /usr/local/heroku/bin
 set -g -x fish_greeting ""
-set -g -x EDITOR /usr/bin/vim
+set -g -x EDITOR ~/.dotfiles/e
+set -g -x GOPATH ~/code/lang/go/gopath
 
 # for vim airline
 # set -g -x LC_ALL en_US.UTF8
--- fish/config.symlink/fish/functions/go.fish
+++
@@ -1,6 +0,0 @@
-function go --description "navigates to directory, clears screen"
-    cd $argv[1]
-    clear
-    pwd
-    l
-end
