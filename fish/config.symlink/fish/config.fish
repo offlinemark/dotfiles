fcd6d123ab9422a0b6d968279063057096fd5b51
Mark Mossberg <mark.mossberg@gmail.com>
2016-07-11 00:33:47 -0400

    fish

--- fish/config.symlink/fish/config.fish
+++ fish/config.symlink/fish/config.fish
@@ -1,10 +1,14 @@
 source ~/.aliases
+source ~/.abbr.fish
 
 # env variables
-set -g -x PATH . /usr/local/bin /usr/local/sbin $PATH ~/.dotfiles/bin /usr/local/heroku/bin
+set -g -x PATH . /usr/local/bin /usr/local/sbin $PATH ~/.dotfiles/bin
 set -g -x fish_greeting ""
-set -g -x EDITOR ~/.dotfiles/e
+set -g -x EDITOR ~/.dotfiles/bin/e
 set -g -x GOPATH ~/code/lang/go/gopath
+set -g -x CFLAGS "-Wall -pedantic"
+
+set -g fish_key_bindings fish_vi_key_bindings
 
 # for vim airline
 # set -g -x LC_ALL en_US.UTF8
